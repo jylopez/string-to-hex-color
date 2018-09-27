1366cc8c1f4227e36c448a4eb46ad213c0e7b50d
Jershon <jershonlopez@gmail.com>
2018-09-27 08:14:48 -0600

    Add semicolon

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = function (str) {
     var r = parseInt(color.slice(0, 2), 16);
     var g = parseInt(color.slice(2, 4), 16);
     var b = parseInt(color.slice(4, 6), 16);
-    var bwComplement = (r * 0.299 + g * 0.587 + b * 0.114) > 186 ? '#000000' : '#FFFFFF'
+    var bwComplement = (r * 0.299 + g * 0.587 + b * 0.114) > 186 ? '#000000' : '#FFFFFF';
   
     return [ color, bwComplement ];
 }
